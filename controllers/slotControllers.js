e28f2a22db1771e67a38a4e2687c9558d60b00fc
Franco Fernandez <fran.fercalderon@gmail.com>
2024-12-19 14:51:39 -0300

    updating searchMatches function

--- controllers/slotControllers.js
+++ controllers/slotControllers.js
@@ -5,17 +5,27 @@ const { postNotification } = require("./notificationControllers")
 const postFixedSlot = async ( req, res ) => {
     try {
         const data = req.body
+        const userId = req.user.uid
         if( !data || Object.keys( data ).length === 0 ) {
             return res.status( 400 ).json( { message: 'Slot data is required in request body.' } ) 
         }
         const slot = {
             ...data,
-            userId: req.user.uid,
+            userId,
             id: v4()
         }
         const docId = await createDocumentInCollection( 'fixedSlots', slot )
+
         if( docId ) {
+            console.log('docId');
             res.status( 201 ).json( docId )
+            console.log('llega?');
+
+            matchRecurringSlots( userId, slot ).catch( error => {
+
+                console.error( 'Error executing matchRecurringSlots:', error )
+            })
+
         } else {
             res.status( 400 ).json( { message: 'Could not post slot.' } )
         }
@@ -227,6 +237,163 @@ const addNewAttendant = async ( req, res ) => {
         res.status( 500 ).json( { message: 'Internal server error.' } )
     }
 }
+
+const recurringTimeToMinutes = ( time ) => {
+
+    const hour = parseInt( time.hour, 10 )  
+    const minute = parseInt( time.minute, 10 )
+    const isPM = time.ampm.toLowerCase() === 'pm'
+
+    const normalizedHour = isPM && hour !== 12 ? hour + 12 : ( isPM || hour === 12 ) ? hour : hour 
+    const totalMinutes =  normalizedHour * 60 + minute
+    
+    return totalMinutes
+}
+
+const minutesToTime = ( minutes ) => {
+
+    const hour24 = Math.floor( minutes / 60 )
+    const minute = minutes % 60
+    const ampm = hour24 >= 12 ? 'pm' : 'am'
+    const hour12 = hour24 % 12 === 0 ? 12 : hour24 % 12
+    
+    return { 
+        hour: hour12.toString(),
+        minute: minute.toString().padStart( 2, '0' ),
+        ampm
+    }
+}
+
+const getOverlapTime = ( start1, end1, start2, end2 ) => {
+
+    if ( start1 < end2 && start2 < end1 ){
+
+        const overlapStart = Math.max( start1, start2 )
+        const overlapEnd = Math.min( end1, end2 )
+
+        const overlapDuration = overlapEnd - overlapStart
+
+        if (overlapDuration < 60) {
+            return null
+        }
+
+        return {
+            overlapStart,
+            overlapEnd
+        }
+
+    } else {
+        return null 
+    }
+}
+
+const findRecurringSlotsMatches = ( userSlot, friendSlots ) => {
+    
+    const matches = []
+
+    const userStart = recurringTimeToMinutes( userSlot.startTime )
+    const userEnd = recurringTimeToMinutes( userSlot.endTime )
+
+    for ( const friendSlot of friendSlots ){
+        
+        const commonDays = userSlot.days.filter(( day ) => friendSlot.days.includes( day ))
+
+        if( commonDays.length > 0 ){
+            
+            const friendStart = recurringTimeToMinutes( friendSlot.startTime )
+            const friendEnd = recurringTimeToMinutes( friendSlot.endTime )
+
+            const overlap = getOverlapTime( userStart, userEnd, friendStart, friendEnd )
+
+            if( overlap ){
+                const overlapStart = minutesToTime( overlap.overlapStart )
+                const overlapEnd = minutesToTime( overlap.overlapEnd )
+
+                matches.push({
+                    commonDays,
+                    userSlot,
+                    friendSlot,
+                    overlapTime: {
+                        starts: overlapStart,
+                        ends: overlapEnd
+                    }
+                })
+            }
+        }
+    }
+
+    return matches
+}
+
+const matchRecurringSlots = async ( userId, newSlot ) => {
+    console.log('corre');
+
+    const userFriends = await getDocsWhereCondition( 'users', 'id', userId )
+
+    const matches = []
+
+    if( userFriends.length > 0 ){
+
+        for ( const friend of userFriends ){
+    
+            const friendRecurringSlots = await getDocsWhereCondition( 'fixedSlots', 'userId', friend.id )
+            if ( friendRecurringSlots.length > 0 ){
+                
+                const slotsWithPriority = friendRecurringSlots.map( slot => ({
+                    ...slot,
+                    priority: friend.priority
+                }))
+
+                const friendMatches = findRecurringSlotsMatches( newSlot, slotsWithPriority )
+
+                if( friendMatches.length > 0 ){
+                    friendMatches.forEach(( match ) => {
+                        matches.push( match )
+                    })
+                }
+
+            }
+    
+        }
+    }
+
+    console.log( matches );
+
+}  
+
+
+
+// const mySlot = {
+//     days: ['thursday', 'friday'],
+//     startTime: { hour: '9', minute: '00', ampm: 'am' },
+//     endTime: { hour: '10', minute: '00', ampm: 'am' },
+//     userId: 'user1',
+// }
+
+// const friendRecurringSlots = [
+//     {
+//         days: ['friday'],
+//         startTime: { hour: '10', minute: '0', ampm: 'am' },
+//         endTime: { hour: '11', minute: '0', ampm: 'am' },
+//         userId: 'friend1',
+//     },
+//     {
+//         days: ['friday'],
+//         startTime: { hour: '8', minute: '0', ampm: 'am' },
+//         endTime: { hour: '11', minute: '0', ampm: 'am' },
+//         userId: 'friend2',
+//     },
+//     {
+//         days: ['saturday'],
+//         startTime: { hour: '9', minute: '0', ampm: 'am' },
+//         endTime: { hour: '10', minute: '0', ampm: 'am' },
+//         userId: 'friend3',
+//     },
+// ];
+
+// const resultado = findRecurringSlotsMatches( mySlot, friendRecurringSlots )
+
+// console.log( resultado )
 
 
 module.exports = {
